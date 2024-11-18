1b52ddeb0422b0f26f3682f0c38f1e25d815d9a2
guybarak <guybarak@checkpoint.com>
2024-11-18 15:17:50 +0200

    only main

--- main.tf
+++ main.tf
@@ -1,12 +1,33 @@
-resource "aws_subnet" "minimal_subnet" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.cidr_block
-  availability_zone = var.availability_zone
+module "minimal_vpc" {
+  source = "github.com/chkp-guybarak/terraform-aws-guy-test1//modules/minimal_vpc?ref=1.1.4"
 
-  tags = merge(
-    {
-      Name = var.name
-    },
-    var.tags
-  )
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  name                 = "example-minimal-vpc"
+  tags = {
+    Environment = "Test"
+    Team        = "DevOps"
+  }
+}
+
+module "minimal_subnet" {
+  source = "github.com/chkp-guybarak/terraform-aws-guy-test1//modules/minimal_subnet?ref=1.1.4"
+
+  vpc_id            = module.minimal_vpc.vpc_id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  name              = "example-minimal-subnet"
+  tags = {
+    Environment = "Test"
+    Team        = "DevOps"
+  }
+}
+
+output "vpc_id" {
+  value = module.minimal_vpc.vpc_id
+}
+
+output "subnet_id" {
+  value = module.minimal_subnet.subnet_id
 }
